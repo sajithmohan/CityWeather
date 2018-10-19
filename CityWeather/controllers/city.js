d8175d8270850f587b7ca8fea2da1d514c81de3e
sajithmohancc <sajithmohan.v.k@cognitiveclouds.com>
2018-10-20 00:53:19 +0530

    API documentation using apidoc syntax

--- CityWeather/controllers/city.js
+++ CityWeather/controllers/city.js
@@ -5,6 +5,23 @@ const City = mongoose.model('City');
 const confg = require('../config.js');
 var errors = require('restify-errors');
 
+/**
+ * @api {get} /cities List
+ * @apiName List
+ * @apiGroup Cities
+ *
+ * @apiParam (QueryParam) {Number} lat Latitude value.
+ * @apiParam (QueryParam) {Number} lon Longitude value.
+ *
+ * @apiSuccess (200) {Number} page current page.
+ * @apiSuccess (200) {Number} pages total pages.
+ * @apiSuccess (200) {Object[]} data cities.
+ * @apiSuccess (200) {String} data.name City name.
+ * @apiSuccess (200) {Number} data.id City id.
+ *
+ * @apiError (400) {String} code error code
+ * @apiError (400) {String} message error message
+ */
 const list = async function(req, res){
     const page = (req.query.page > 0 ? req.query.page : 1) - 1;
     const limit = confg.defaultPageLimit;
@@ -33,6 +50,24 @@ const list = async function(req, res){
     res.send(200, response);
 };
 
+/**
+ * @api {get} /cities/:id Details
+ * @apiName Details
+ * @apiGroup Cities
+ *
+ * @apiParam  {Number} id City id.
+ *
+ * @apiSuccess {Object} data City.
+ * @apiSuccess {Object} data.coord Coordnates
+ * @apiSuccess {Number} data.coord.lon Longitude.
+ * @apiSuccess {Number} data.coord.lat Latitude.
+ * @apiSuccess {Number} data.id City id.
+ * @apiSuccess {String} data.name Name.
+ * @apiError (400) {String} code error code
+ * @apiError (400) {String} message error message
+ * @apiError (404) {String} code error code
+ * @apiError (404) {String} message error message
+ */
 const details = async function(req, res){
     const id = req.params.id;
     let city = await City.details(id);
--- CityWeather/controllers/weather.js
+++ CityWeather/controllers/weather.js
@@ -2,11 +2,37 @@
 
 const CityWeather = require('../lib/cityweather.js');
 
+/**
+ * @api {get} /cities/:id/weather Details
+ * @apiName weather Details
+ * @apiGroup Weather
+ *
+ * @apiParam  {Number} id City id.
+ *
+ * @apiSuccess {Object} data Weather.
+ * @apiSuccess {String} data.type type
+ * @apiSuccess {String} data.type_description description
+ * @apiSuccess {String} data.sunrise sunrise
+ * @apiSuccess {String} data.sunset sunset
+ * @apiSuccess {Number} data.temp temp.
+ * @apiSuccess {Number} data.temp_min temp_min.
+ * @apiSuccess {Number} data.temp_max temp_max.
+ * @apiSuccess {Number} data.pressure pressure.
+ * @apiSuccess {Number} data.humidity humidity.
+ * @apiSuccess {Number} data.clouds_percent clouds_percent.
+ * @apiSuccess {Number} data.wind_speed wind_speed.
+ *
+ * @apiError (400) {String} code error code
+ * @apiError (400) {String} message error message
+ * @apiError (404) {String} code error code
+ * @apiError (404) {String} message error message
+ */
+
 const list = async (req, res) =>{
     const cityId = req.params.id;
     var cw = new CityWeather(cityId);
     await cw.load();
-    res.send(200, cw);
+    res.send(200, {data: cw});
 };
 
 module.exports = {
